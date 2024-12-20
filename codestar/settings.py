1f2f836e2cb9718edf3d25c8787fc3265e948f88
olletidblom <4xnxsrzc@students.codeinstitute.net>
2024-12-20 12:04:41 +0100

    Reworked

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-53+w9_h2#!fuucgimc*g31(m1ua)ssph+jxjywhi-n372dpnh5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',]
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    '.herokuapp.com',
+
 ]
 
 MIDDLEWARE = [
