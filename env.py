ce7f0d70d68cda5cf2bbbf2542f9fbe72b80edfc
olletidblom <4xnxsrzc@students.codeinstitute.net>
2024-12-20 13:42:37 +0100

    Added secret key

--- env.py
+++ env.py
@@ -4,3 +4,8 @@ os.environ.setdefault(
     "DATABASE_URL", 
 "postgresql://neondb_owner:HgbfuWeV1JL6@ep-shy-recipe-a2bwveb5.eu-central-1.aws.neon.tech/navy_baton_crowd_113811"
 )
+
+os.environ.setdefault(
+    "SECRET_KEY",
+   "T9IxTAr7whhvjEL1qN4nlpB0tcgdDWfV"
+)
