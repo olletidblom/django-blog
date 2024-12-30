043c518a48aa8c218c91204d11d99f6af19f78bd
olletidblom <4xnxsrzc@students.codeinstitute.net>
2024-12-30 13:36:15 +0100

    added post_detail vie updatew

--- blog/views.py
+++ blog/views.py
@@ -10,23 +10,25 @@ class PostList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6
     
-def post_detail(request, slug): 
+def post_detail(request, slug):
     """
-        Display an individual :model:`blog.Post`.
+    Display an individual :model:`blog.Post`.
 
-        **Context**
+    **Context**
 
-        ``post``
-            An instance of :model:`blog.Post`.
+    ``post``
+        An instance of :model:`blog.Post`.
 
-        **Template:**
+    **Template:**
 
-        :template:`blog/post_detail.html`
-     """
-     
+    :template:`blog/post_detail.html`
+    """
+
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    return render (
-        request, "blog/post_detail.html",
+
+    return render(
+        request,
+        "blog/post_detail.html",
         {"post": post},
     )
