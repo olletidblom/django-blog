7fb418b8a99ed3688baf59c570787efe955239a2
olletidblom <4xnxsrzc@students.codeinstitute.net>
2025-01-07 22:52:40 +0100

    added worked on comments

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 
 from django.views import generic
 from .models import Post
-
+from .forms import CommentForm
+from django.contrib import messages
 # Create your views here.
 
 class PostList(generic.ListView):
@@ -11,30 +12,33 @@ class PostList(generic.ListView):
     paginate_by = 6
     
 def post_detail(request, slug):
-    """
-    Display an individual :model:`blog.Post`.
-
-    **Context**
-
-    ``post``
-        An instance of :model:`blog.Post`.
-
-    **Template:**
-
-    :template:`blog/post_detail.html`
-    """
-
+  
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
-
+    
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval')
+    comment_form = CommentForm()
+    
     return render(
         request,
         "blog/post_detail.html",
         {"post": post,
          "comments": comments,
-         "comment_count": comment_count 
+         "comment_count": comment_count,
+         "comment_form": comment_form,
+         
          },
 
 
--- codestar/settings.py
+++ codestar/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'django_summernote',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'blog',
     'about',
 
@@ -56,8 +58,8 @@ SITE_ID = 1
 LOGIN_REDIRECT_URl = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-
-
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
